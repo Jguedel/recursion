482d669249d9965d252ed587f08d36965ea7630d
Jacob Guedel <jhguedel@amazon.com>
2023-07-25 13:51:25 -0700

    7/25/2023

--- recursion.js
+++ recursion.js
@@ -30,23 +30,22 @@ function longest(words) {
 //had to add variable in function call
 function everyOther(str, n = 0, ans = "") {
   if (str.length <= 1) {
-    if (n%2===0) {
-      ans+= str.substring(0,1)
-      return ans
+    if (n % 2 === 0) {
+      ans += str.substring(0, 1);
+      return ans;
     } else {
       return ans;
     }
   } else {
-    if (n%2 === 0) {
+    if (n % 2 === 0) {
       n++;
-      ans += str.substring(0,1)
-      str = str.slice(1,str.length)
-      return everyOther(str,n,ans)
-      
+      ans += str.substring(0, 1);
+      str = str.slice(1, str.length);
+      return everyOther(str, n, ans);
     } else {
       n++;
-      str = str.slice(1,str.length)
-      return everyOther(str,n,ans)
+      str = str.slice(1, str.length);
+      return everyOther(str, n, ans);
     }
   }
 }
@@ -55,65 +54,60 @@ function everyOther(str, n = 0, ans = "") {
 
 function isPalindrome(str) {
   if (str.length <= 1) {
-    return true
+    return true;
   } else {
-    let start = str[0]
-    let end = str[(str.length-1)]
+    let start = str[0];
+    let end = str[str.length - 1];
     if (start === end) {
-      str = str.slice(1,str.length-1)
-      return isPalindrome(str)
+      str = str.slice(1, str.length - 1);
+      return isPalindrome(str);
     } else {
-      return false
+      return false;
     }
-    
   }
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 //had to add variable in function call
-function findIndex(arr, val,index = 0) {
-  if (index === arr.length-1) {
+function findIndex(arr, val, index = 0) {
+  if (index === arr.length - 1) {
     if (arr[index] === val) {
-      return index
-    }
-    else{
-      return -1
+      return index;
+    } else {
+      return -1;
     }
   } else {
     if (arr[index] === val) {
-      return index
-    }
-    else{
-      index = index+1
-      return findIndex(arr,val,index)
+      return index;
+    } else {
+      index = index + 1;
+      return findIndex(arr, val, index);
     }
   }
 }
 
 /** revString: return a copy of a string, but in reverse. */
 //had to add variable in function call
-function revString(str,ans = '') {
+function revString(str, ans = "") {
   if (str.length === 1) {
-    ans += str
-    return ans
-  }
-  else{
-    ans += str.substring(str.length-1,str.length)
-    str = str.slice(0,str.length-1)
-    return revString(str,ans)
+    ans += str;
+    return ans;
+  } else {
+    ans += str.substring(str.length - 1, str.length);
+    str = str.slice(0, str.length - 1);
+    return revString(str, ans);
   }
 }
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
 function gatherStrings(obj) {
-  let arrString = []
-  for(let i in obj){
-    if (typeof obj[i] === 'string') {
-      arrString.push(obj[i])
-    }
-    else if(typeof obj[i] === 'object'){
-      arrString = arrString.concat(gatherStrings(obj[i]))
+  let arrString = [];
+  for (let i in obj) {
+    if (typeof obj[i] === "string") {
+      arrString.push(obj[i]);
+    } else if (typeof obj[i] === "object") {
+      arrString = arrString.concat(gatherStrings(obj[i]));
     }
   }
   return arrString;
@@ -122,19 +116,16 @@ function gatherStrings(obj) {
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
 
-function binarySearch(arr, val, start = 0, end = (arr.length-1)) {
-  let midPoint = Math.floor(((end-start)/2) + start)
+function binarySearch(arr, val, start = 0, end = arr.length - 1) {
+  let midPoint = Math.floor((end - start) / 2 + start);
   if (arr[midPoint] === val) {
-    return midPoint
-  }
-  else if(end - start === 0){
-    return -1
-  }
-  else if(arr[midPoint] < val){
-    return binarySearch(arr, val, midPoint+1, end)
-  }
-  else if(arr[midPoint] > val){
-    return binarySearch(arr, val, start, midPoint)
+    return midPoint;
+  } else if (end - start === 0) {
+    return -1;
+  } else if (arr[midPoint] < val) {
+    return binarySearch(arr, val, midPoint + 1, end);
+  } else if (arr[midPoint] > val) {
+    return binarySearch(arr, val, start, midPoint);
   }
 }
 
